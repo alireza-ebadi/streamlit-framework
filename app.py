ebae5eea60deb25637f037b535d5b350b769aac4
Alireza Ebadi <alireza.ebadi@unh.edu>
2021-04-07 15:43:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import numpy as np
 FS = 24
 FN = 'times new roman'
 font = {'family':'serif', 'size':FS}
-plt.rc('text', usetex=True)
 plt.rc('font', **font)
 
 st.title('Define stock parameters')
@@ -20,10 +19,11 @@ month = st.text_input('Month (e.g. 3 or March):')
 year = st.text_input('Year (e.g. 2019):')
 
 test = np.linspace(1, 100, 30)
-plt.figure(figsize=(10, 6))
-plt.ylabel('USD')
-plt.xlabel('Date')
-st.pyplot()
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(test, test*2)
+ax.ylabel('USD')
+ax.xlabel('Date')
+st.pyplot(fig)
 
 
 """
