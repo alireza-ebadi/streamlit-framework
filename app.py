eb1743ac674d5353950ecf840a4fcbea8898c651
Alireza Ebadi <alireza.ebadi@unh.edu>
2021-04-07 11:20:24 -0400

    Demo

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from alpha_vantage.timeseries import TimeSeries
+
+FS = 24                # font size
+FN = 'times new roman' # font name
+font = {'family':'serif', 'size':FS}
+plt.rc('text', usetex=True)
+plt.rc('font', **font)
+
+myKey = os.getenv('ALPHAVANTAGE_API_KEY')
+
+ticker = input('ticker symbol: ')
+month =  input('month: ')
+year = input('year: ')
+
+
+ts = TimeSeries(key=myKey, output_format='pandas')
+data, meta_data = ts.get_daily_adjusted(symbol=ticker, outputsize='full')
+
+plt.figure(figsize=(10, 6))
+data['4. close'].loc[year+'-'+month].plot()
+plt.title('Closing price in ' + month.capitalize() + '-' + year +' for the ' + ticker.upper() + ' stock')
+plt.ylabel('USD')
+plt.xlabel('Date')
+plt.show()
